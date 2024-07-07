ebba62683365ed94e5fd9deff0991150c85a9243
Norluis Galvez <norly021196@gmail.com>
2024-07-07 16:22:51 -0400

    show html

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ const port = process.env.PORT || 3002
 const V1SwaggerDocs = swaggerDocs
 
 connectDB();
+app.get("/", (req, res) => {
+    const htmlResponse = `
+      <html>
+        <head> 
+           <title>MERN-TASK-BACKEND</title>
+        </head>
+        <body>
+           <h1>MERN-TASK-BACKEND</h1>
+        </body>
+      </html>
+    `;
+    res.send(htmlResponse)
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
     V1SwaggerDocs(app, port)
