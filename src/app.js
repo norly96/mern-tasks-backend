92a35b8569a63725631cb10f769a17fdaacbf80c
Norluis Galvez <norly021196@gmail.com>
2024-07-07 23:49:00 -0400

    fix cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(morgan('dev'))
 app.use(express.json()) //Read the objects JSON
 app.use(cookieParser())
