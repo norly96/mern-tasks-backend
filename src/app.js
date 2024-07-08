b5e501cd525d45fac8380f5275e64a6b0b95e0fb
Norluis Galvez <norly021196@gmail.com>
2024-07-07 23:24:54 -0400

    add cors

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import morgan from "morgan"
 import authRoutes from "./routes/auth.routes.js"
 import tasksRoutes from "./routes/tasks.routes.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json()) //Read the objects JSON
 app.use(cookieParser())
