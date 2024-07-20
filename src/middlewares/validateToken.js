00c55f45a02fcc425b0c84e3fb7c4be7138eb8af
Norluis Galvez <norly021196@gmail.com>
2024-07-19 21:43:07 -0400

    change send cookie by headers

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,11 +2,14 @@ import { TOKEN_SECRET } from "../config.js"
 import jwt from "jsonwebtoken"
 
 export const authRequired = (req, res, next) => {
-    const { token } = req.cookies
-    if(!token) return res.status(401).json({message: "No token, authorization denied"})
-        jwt.verify(token, TOKEN_SECRET, (err, user) => {
-    if(err) return res.status(403).json({message: "Invalid token"}) 
-    req.user = user 
-    next()
-})
-}
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) return res.status(401).json({ message: ["No token, authorization denied"] })
+
+    jwt.verify(token, TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: ["Invalid token"] });
+        req.user = user;
+        next();
+    });
+};
